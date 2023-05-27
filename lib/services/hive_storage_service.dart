8964c542bd3e26ad5e05ffed0a2bb0f4dda75849
Finn Jesse Dan <83787516+Jesse-Dan@users.noreply.github.com>
2023-05-27 12:39:58 +0100

    created a hive local sttorage class

---
+++ lib/services/hive_storage_service.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/foundation.dart';
+import 'package:hive/hive.dart';
+import 'package:rex/screens/cart_screen/models/Gaz.dart';
+
+late Box<Gaz> gazBox;
+
+class HiveHelper {
+  deleteInfo(int index) {
+    gazBox.deleteAt(index);
+    if (kDebugMode) {
+      print('Item deleted from box at index:$index');
+    }
+  }
+
+  List<Gaz> getDataList() {
+    final dataList = gazBox.values.toList();
+    return dataList;
+  }
+
+  deleteAll() {
+    gazBox.clear();
+  }
+
+  totalItems() {
+    var data = getDataList();
+    if (data.isEmpty) {
+      return 0;
+    }
+    var total = (data.fold<int?>(data.first.price,
+        (previousValue, element) => previousValue! + element.price));
+    return (total);
+  }
+}
